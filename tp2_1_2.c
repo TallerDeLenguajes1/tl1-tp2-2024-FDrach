9fb59371f2e07d8cecb4a2c3b85e9a4234423c79
Franco Drachenberg <Francodrach@gmail.com>
2024-04-09 15:12:54 -0300

    rama2

---
+++ tp2_1_2.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define N 20
+
+int main() {
+    srand(time(NULL));
+    int i;
+    double vt[N];
+    printf(" ");
+    for(i = 0;i<N; i++)
+    {
+        *(vt + i) = 1 + rand() % 100;
+        printf("%f\n ", *(vt + i));
+    }
+    return 0;
+}
